dfacdf01bd6672958800c7c1d658e9937a7695c4
biere <biere@informatik.uni-freiburg.de>
2023-03-05 13:41:41 +0100

    some error checking for fuzzer

--- fuzzing/run.sh
+++ fuzzing/run.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+die () {
+  echo "run.sh: error: $*" 1>&2
+  exit 1
+}
+
+[ -f ../cadiback ] || \
+  die "no '../cadiback' found (compile it first)"
+
+[ x"`runcnfuzz -h 2>/dev/null`" = x ] && \
+  die "could not find 'runcnfuzz' (install 'runcnfuzz', 'cnfuzz' and 'cnfdd' first)"
+
 . ./options.sh
 
 for o1 in $options
 do
-  rest=`echo $options|sed -e s,^.*$o1,,`
+  rest="`echo $options|sed -e s,^.*$o1,,`"
   [ "$rest" = "" ] && break
   for o2 in $rest
   do
@@ -22,6 +33,10 @@ do
     fi
     dir=${o1}-${o2}
     echo $dir$args
+    mkdir $dir 2>/dev/null || \
+      die "could not make directory '$dir' (did you run 'make clean' first)"
+    cd $dir
+    cd ..
   done
 done
 
